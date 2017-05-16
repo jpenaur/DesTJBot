1e42a1a96c83a90e2bc0727550fb6181f6780bd9
jpenaur <JPena_Ur@es.ibm.com>
2017-05-16 13:20:06 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,24 @@
+// The attention word to wake up the robot.
+exports.attentionWord ='watson';
+
+// You can change the voice of the robot to your favorite voice.
+exports.voice = 'en-US_MichaelVoice';
+// Some of the available options are:
+// en-US_AllisonVoice
+// en-US_LisaVoice
+// en-US_MichaelVoice (the default)
+
+// Credentials for Watson Speech to Text service
+exports.STTUsername = '9932e20a-565e-4b3c-b014-fb4e096acebd' ;
+exports.STTPassword = 'hMxORPtKAmX4' ;
+
+//Credentials for Watson Weather Insights service
+exports.WEATHERURL = 'twcservice.mybluemix.net';
+exports.WEATHERUsername = '4d79a97d-9138-4173-a10c-a3b4506e0772';
+exports.WEATHERPassword = 'ySWVvC1NaQ';
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = 'af91eb5c-927a-4a90-98fa-31ec816d4f91' ;
+exports.TTSPassword = 'ZwPe1q04EMAL' ; 
+
+exports.version = 'v1';
