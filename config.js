3a906e72eceb6506a798593d70053cfab0785f91
jpenaur <JPena_Ur@es.ibm.com>
2017-05-20 14:41:38 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+// The attention word to wake up the robot.
+exports.attentionWord ='watson';
+
+// You can change the voice of the robot to your favorite voice.
+exports.voice = 'es-ES_LauraVoice';
+
+
+// Credentials for Watson Speech to Text service
+exports.STTUsername = 'e726faf3-3c7c-4c22-833c-92b125ab17d5';
+exports.STTPassword = 'rljMKslxEiVy';
+
+// Credentials for Watson Conversation service
+exports.ConUsername = '417a4417-1390-4406-ad19-6b364d8b276e';
+exports.ConPassword = 'jUTUtlva5RHK';
+exports.ConWorkspace = '72672e14-8cea-4a1b-95b9-0c70fc25e933'
+
+//Credentials for Watson Tone Analyzer service
+exports.ToneUsername = '762c6ea9-b435-468f-b809-8751ee21bd1d';
+exports.TonePassword = 'kmq7UBItc676';
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = 'f0216f95-d8cf-4834-ad33-d9d55f34ca23';
+exports.TTSPassword = 'D3qf58ZSGd8C';
